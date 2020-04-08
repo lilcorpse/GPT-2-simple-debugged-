22cbaf4f929f7fd6c18d40fc56d05fd18167c5c9
lilcorpse <63360666+lilcorpse@users.noreply.github.com>
2020-04-08 19:06:42 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import sys
 import codecs
 
 
-model_name = "345M"
+model_name = "1558M"
 if not os.path.isdir(os.path.join("models", model_name)):
         print(f"Downloading {model_name} model...")
         gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
